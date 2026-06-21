16a5265e33200f9b45f4cad45637837392db792b
5vpb6rrmv4-wq <5vpb6rrmv4@privaterelay.appleid.com>
2026-06-21 23:06:47 +0800

    Add backend/src/main/java/com/cqupt/ftcp/FtcpApplication.java

---
+++ backend/src/main/java/com/cqupt/ftcp/FtcpApplication.java
@@ -0,0 +1,11 @@
+package com.cqupt.ftcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtcpApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FtcpApplication.class, args);
+    }
+}
